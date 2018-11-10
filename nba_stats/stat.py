65c8d47abf3e2b47bf9b1397e07c734e774afdcf
gorioboy15 <gortol15@gmail.com>
2018-11-10 09:52:53 -0800

    upd stat.py

--- nba_stats/stat.py
+++ nba_stats/stat.py
@@ -10,6 +10,7 @@ with open('nba_names.txt', 'r') as n:
 with open('nba_standings_stat.txt','r')as f:
    stat = f.read()
 
+# filter text and nums in stat
 pat = re.compile(r'[0-9\-\.WL]+')
 res = re.findall(pat,stat)
 cnt = 14
@@ -17,9 +18,10 @@ ct = 0
 fin = []
 f = []
 snf = ''
+# split string by len 13 and create list by it
 for i in res:
    ct += 1
-   if ct != cnt:
+   if ct != cnt: #
       f.append(i)
    elif ct == cnt:
       fin.append(f)
@@ -28,19 +30,20 @@ for i in res:
       f.append(i)
 cnt = 0
 
-#print(len(fin[0][3])) # print it in order and aligned
-for i in fin:
-   for e in i:
-      if e == i[3] and len(e) == 1:
-         i[3] = f'{i[3]}  '
-      elif e == i[9] and len(e) == 4:
-         i[9] = f'{i[9]} '
-      elif e == i[10] and len(e) == 3:
-         i[10] = f'{i[10]}  '
-      elif e == i[10] and len(e) == 4:
-         i[10] = f'{i[10]} '
-      else:
-         continue
+#print(len(fin[0][3]))
+#  print it in order and aligned
+# for i in fin:
+#    for e in i:
+#       if e == i[3] and len(e) == 1:
+#          i[3] = f'{i[3]}  '
+#       elif e == i[9] and len(e) == 4:
+#          i[9] = f'{i[9]} '
+#       elif e == i[10] and len(e) == 3:
+#          i[10] = f'{i[10]}  '
+#       elif e == i[10] and len(e) == 4:
+#          i[10] = f'{i[10]} '
+#       else:
+#          continue
 
 
 cnt = 0
